1bc576f7b6e599ce60f3d90446866ea6ccb9c7b1
Davide Chiapperini <davide.chiapperinithebest@hotmail.it>
2021-06-12 17:21:15 +0200

    Rim bottoni

--- ParenteApp/app/src/main/java/com/example/parenteapp/MainActivity.java
+++ ParenteApp/app/src/main/java/com/example/parenteapp/MainActivity.java
@@ -61,17 +61,8 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
 
         //Globals.myAppID = 100;
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
 
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "1");
