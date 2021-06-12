b0ee387edd1d75444eb9bd88c64b3f0374ccb5c9
Davide Chiapperini <davide.chiapperinithebest@hotmail.it>
2021-06-12 17:24:36 +0200

    Rim bottoni

--- PepperApp/app/src/main/java/com/example/pepperapp28aprile/ProfileActivity.java
+++ PepperApp/app/src/main/java/com/example/pepperapp28aprile/ProfileActivity.java
@@ -45,16 +45,6 @@ public class ProfileActivity extends RobotActivity implements RobotLifecycleCall
             System.out.println("una volta");
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-       fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
